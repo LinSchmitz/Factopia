17fbbd1562b330cd03e0aa66ea3be8d6e114111e
naleilan <leilan.n@gmail.com>
2025-06-30 12:34:41 +0330

    Added some css

--- data.js
+++ data.js
@@ -43,6 +43,11 @@ const initialFacts = [
   },
 ];
 
-// LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
+// LINK TO APP SAMPLE DATA:
 
 // 👍 🤯 ⛔️
+// supabase: Factopia / 322792
+
+//  curl.exe "https://hamcgcpwtbzfjrzqqsie.supabase.co/rest/v1/facts" `
+// -H "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhbWNnY3B3dGJ6ZmpyenFxc2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE3NzEsImV4cCI6MjA2Njg0Nzc3MX0.Gb4r9HExetrC3uXNkuMacCKr7GNwU14P8Vwf4OjWCEQ" \`
+// -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhbWNnY3B3dGJ6ZmpyenFxc2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE3NzEsImV4cCI6MjA2Njg0Nzc3MX0.Gb4r9HExetrC3uXNkuMacCKr7GNwU14P8Vwf4OjWCEQ"
