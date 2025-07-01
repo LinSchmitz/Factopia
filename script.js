d01ed8df7e0b1b83e11a03be09add6b38994ab32
naleilan <leilan.n@gmail.com>
2025-07-01 09:18:14 +0330

    Added api

--- script.js
+++ script.js
@@ -58,10 +58,12 @@ async function loadFacts() {
   const res = await fetch(
     'https://hamcgcpwtbzfjrzqqsie.supabase.co/rest/v1/facts',
     {
-      apikey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhbWNnY3B3dGJ6ZmpyenFxc2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE3NzEsImV4cCI6MjA2Njg0Nzc3MX0.Gb4r9HExetrC3uXNkuMacCKr7GNwU14P8Vwf4OjWCEQ',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhbWNnY3B3dGJ6ZmpyenFxc2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE3NzEsImV4cCI6MjA2Njg0Nzc3MX0.Gb4r9HExetrC3uXNkuMacCKr7GNwU14P8Vwf4OjWCEQ',
+      headers: {
+        apikey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhbWNnY3B3dGJ6ZmpyenFxc2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE3NzEsImV4cCI6MjA2Njg0Nzc3MX0.Gb4r9HExetrC3uXNkuMacCKr7GNwU14P8Vwf4OjWCEQ',
+        authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhbWNnY3B3dGJ6ZmpyenFxc2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE3NzEsImV4cCI6MjA2Njg0Nzc3MX0.Gb4r9HExetrC3uXNkuMacCKr7GNwU14P8Vwf4OjWCEQ',
+      },
     }
   );
   const data = await res.json();
