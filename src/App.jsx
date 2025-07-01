deefdfa6753655369ad80e118c4690e6abc25e88
naleilan <leilan.n@gmail.com>
2025-07-01 12:54:44 +0330

    add some components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,36 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <header className="header">
-        <div className="logo">
-          <img src="/img/logo.png" alt="logo" />
-          <h1>Factopia</h1>
-        </div>
-        <button className="btn btn-large btn-open">Share a fact</button>
-      </header>
+      <Header />
+      <NewFactForm />
+      <main class="main">
+        <FactList />
+        <CategoryFilter />
+      </main>
     </div>
   );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/img/logo.png" alt="logo" />
+        <h1>Factopia</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
+
+function NewFactForm() {
+  return <form class="fact-form">NewFactForm</form>;
+}
+
+function CategoryFilter() {
+  return <aside>CategoryFilter</aside>;
+}
+
+function FactList() {
+  return <section>FactList</section>;
 }
