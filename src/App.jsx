1d09ebb4b63fb8b8a711009b78bab875c92fdafa
naleilan <leilan.n@gmail.com>
2025-07-01 12:40:27 +0330

    Added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div></div>;
-}
+import React from 'react';
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <header className="header">
+        <div className="logo">
+          <img src="/img/logo.png" alt="logo" />
+          <h1>Factopia</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+    </div>
+  );
+}
