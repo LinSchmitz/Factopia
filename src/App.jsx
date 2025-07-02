22593d271de0536dac90e597bdd2119583a20503
naleilan <leilan.n@gmail.com>
2025-07-02 14:57:29 +0330

    added states

--- src/App.jsx
+++ src/App.jsx
@@ -94,17 +94,46 @@ function NewFactForm() {
   const [text, setText] = useState('');
   const [source, setSource] = useState('');
   const [category, setCategory] = useState('');
+  const textLength = text.length;
+
+  function isValidUrl(string) {
+    try {
+      new URL(string);
+      return true;
+    } catch (_) {
+      return false;
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+
+    if (text && isValidUrl(source) && category && text.length <= 500) {
+      const newFact = {
+        id: Math.round(Math.random() * 100000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getUTCFullYear(),
+      };
+    }
+  }
 
   return (
-    <form className="fact-form">
+    <form className="fact-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Share a fact with the world..."
         value={text}
         onChange={e => setText(e.target.value)}
       />
-      <span>200</span>
+      <span>{500 - textLength}</span>
       <input
+        rel="noopener noreferrer"
         type="text"
         placeholder="Trustworthy source..."
         value={source}
@@ -156,11 +185,11 @@ function CategoryFilter() {
 }
 
 function FactList() {
-  const facts = initialFacts;
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <section>
-      <ul className="facts-list" v>
+      <ul className="facts-list">
         {facts.map(fact => (
           <Fact fact={fact} key={fact.id} />
         ))}
