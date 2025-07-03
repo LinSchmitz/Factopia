3c3c33693d860e31c16febfc54809597d7c37958
naleilan <leilan.n@gmail.com>
2025-07-03 09:52:28 +0330

    Handling Votes and Updating a Fact on Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -274,11 +274,11 @@ function FactList({ facts, setFacts }) {
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
 
-  async function handleVote() {
+  async function handleVote(columnName) {
     setIsUpdating(true);
     const { data: updatedFact, error } = await supabase
       .from('facts')
-      .update({ votesInteresting: fact.votesInteresting + 1 })
+      .update({ [columnName]: fact[columnName] + 1 })
       .eq('id', fact.id)
       .select();
     setIsUpdating(false);
@@ -307,11 +307,21 @@ function Fact({ fact, setFacts }) {
         {fact.category}
       </span>
       <div className="vote-button">
-        <button onClick={handleVote} disabled={isUpdating}>
-          👍{fact.votesInteresting}{' '}
+        <button
+          onClick={() => handleVote('votesInteresting')}
+          disabled={isUpdating}
+        >
+          👍{fact.votesInteresting}
         </button>
-        <button>🤯 {fact.votesMindblowing} </button>
-        <button>⛔️ {fact.votesFalse} </button>
+        <button
+          onClick={() => handleVote('votesMindblowing')}
+          disabled={isUpdating}
+        >
+          🤯 {fact.votesMindblowing}
+        </button>
+        <button onClick={() => handleVote('votesFalse')} disabled={isUpdating}>
+          ⛔️ {fact.votesFalse}
+        </button>
       </div>
     </li>
   );
