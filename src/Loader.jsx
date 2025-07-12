4bdfdde95adc3d195a116f3fba6be14a0eecf899
naleilan <leilan.n@gmail.com>
2025-07-12 07:10:48 +0330

    added loading

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export function Loader() {
-  return <p className="message">Loading Data...</p>;
+  return (
+    // <p className="message">
+    <div className="loader">
+      {[...Array(12)].map((_, i) => (
+        <div key={i} className="loader_item" style={{ '--i': i + 1 }}></div>
+      ))}
+    </div>
+    // </p>
+  );
 }
